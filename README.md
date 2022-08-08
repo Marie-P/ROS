543003490242b103d2e150a75d5c85590d96142c
marie-p <mpbwelle@gmail.com>
2022-08-08 08:31:06 +0200

    detect balles

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ROS - Doc d'installation
+
+## Test pour detecter balle corail
+Premier terminal : roscore
+Deuxième terminal : roslaunch turtlebot3_gazebo turtlebot3_demo.launch 
+Troisième terminal : rosrun robot_walk detect_light_coral_object.py
+
+## Test dans la maison
+Premier terminal : roscore
+Deuxième terminal : roslaunch turtlebot3_gazebo turtlebot3_house_demo.launch 
---
+++ src/robot_walk/scripts/move.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import rospy
+# import manip
+# import laser
+from sensor_msgs.msg import LaserScan
+from open_manipulator_msgs.srv import SetJointPosition, SetJointPositionRequest
+# import time
+
+
+
+def callback(msg):
+  print("Laser bras :", msg.ranges[0])
+  rospy.wait_for_service('/goal_joint_space_path')
+  goal_joint_space_path_service_client = rospy.ServiceProxy('/goal_joint_space_path', SetJointPosition)
+  goal_joint_space_path_request_object = SetJointPositionRequest()
+
+  # 1st Movement
+  goal_joint_space_path_request_object.planning_group = 'arm'
+  goal_joint_space_path_request_object.joint_position.joint_name = ['joint1', 'joint2', 'joint3', 'joint4']
+  # goal_joint_space_path_request_object.joint_position.position = [0.0, -1, 0.3, 0.7]
+  goal_joint_space_path_request_object.joint_position.position = [0.0, 0.5, 0.0, 0.0]
+  goal_joint_space_path_request_object.joint_position.max_accelerations_scaling_factor = 1.0
+  goal_joint_space_path_request_object.joint_position.max_velocity_scaling_factor = 1.0
+  goal_joint_space_path_request_object.path_time = 2.0
+  result = goal_joint_space_path_service_client(goal_joint_space_path_request_object)
+
+  # print(msg.data)
+
+
+def callback2(msg):
+  print("Laser robot :", msg.ranges[0])
+  
+
+if __name__=="__main__":
+  rospy.init_node('Mouvement')
+  laserBras_sub=rospy.Subscriber('/scan2',LaserScan,callback)
+  laserRobot_sub=rospy.Subscriber('/scan',LaserScan,callback2)
+  try:
+      rospy.spin()
+  except KeyboardInterrupt:
+      print("Shutting down")
